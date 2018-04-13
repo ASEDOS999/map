45f9b7c234c4d4f49c8e371da276b448b132c005
TimurIbr <36293367+TimurIbr@users.noreply.github.com>
2018-04-13 11:46:19 +0300

    Add files via upload
    Переделанный под входящий файл и более полиморфный вариант.
    Входящий файл - либо 1ый аргумент либо input.txt

--- Test_Tree/usabilite_test.c
+++ Test_Tree/usabilite_test.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "../tree.h"
-
+#include "tree.h"
+#include <string.h>
+#include <ctype.h>
+#define wordsize 80
 struct record{
 	char* lable;
 	int number;
@@ -14,32 +16,70 @@ int rec_cmp(data* a, data* b){
 	if(strcmp(ra->lable, rb->lable) == 0){
 		ra->number = ra->number + 1;
 	}
-	return (strcmp(ra->lable, rb->lable));
+	return( -1) * (strcmp(ra->lable, rb->lable));
 }
-
+#define dFIN "stdin.txt"
 int main(int ac, char** av){
-	int i;
+	char* FIN;
+	if(ac == 1)
+		FIN = dFIN;
+	else 
+		FIN = av[1];
+	FILE *fr = fopen( FIN, "r");
+//	FILE *fw = fopen("stdout.txt", "w");
+	if (fr == NULL)
+	{
+			printf("Eror: input file \"%s\"is not found\n", FIN);
+		return 0;
+	}
+//	printf("Ok\n");
+	int i, j;
+	int c;
 	rec* tmp;
 	map_enter* me;
 	map* dict = tree_create(rec_cmp);
-	for(i = 1; i < ac; ++i){
+	for(c = fgetc(fr); c != EOF; ){
+		while((!isalnum(c)) && (c != EOF))
+			c = fgetc(fr);
 		tmp = malloc(sizeof(rec));
-		tmp->lable = av[i];
+		tmp->lable = malloc(wordsize);
+		j = 0;
+//		printf("\n_");
+		for(; c != EOF; c = fgetc(fr)){
+			/*printf("\\%c/", c);*/
+			if(isalnum(c))
+			{
+				tmp->lable[j] = c;
+			}
+			else{
+				//c = fgetc(fr);
+				break;
+			}
+		j++;
+		}
+		tmp->lable[j] = 0;
+		if(j == 0){
+			free(tmp->lable);
+			free(tmp);
+			//printf("NUL`\n");
+			continue;
+		}
+		
+		//printf("_\n");
+		//printf("_%s_\n", tmp->lable);
 		tmp->number = 1;
 		me = dict->insert(dict, (data*)tmp);
-		if(((branch *)me)->value != (data*)tmp)
-			free(tmp);
 	}
 	for(me = dict->first(dict); 
 		me != dict->end(dict); 
 		me = dict->get_next(dict, me))
-		{
-		printf("%s %d times\n", 
-			((rec*)(((branch *)me)->value))->lable, 
-			((rec*)(((branch *)me)->value))->number
+	{
+		printf("\"%s\" %d times\n", 
+			((rec*)(me->get_val(me)))->lable, 
+			((rec*)(me->get_val(me)))->number
 			);
-		
 	}
+	dict->delet(dict);	
 	return 0;
 }
 
