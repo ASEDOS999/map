cf35251f8b5e4782f6660865295df9f55f3c829a
ased <kuruzov2014@mail.ru>
2018-05-24 04:13:44 +0300

    Change work of hash table: work with keys is an user's task

--- hash.c
+++ hash.c
@@ -1,8 +1,8 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include"hash.h"
-#include<errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "hash.h"
+#include <errno.h>
 
 #define DEBAG
 #ifdef DEBAG
@@ -10,13 +10,9 @@
 	#define ELEM_CHECK
 #endif
 
-#define H 3089 //it is parametеr of hash function
 
-int hashing(data* key){		//it is hash function
-	int* k = (int*)key;
-	if(key == NULL)
-		return 0;
-	return ((*k)%H);
+data* get_value(struct map_enter* el){
+	return ((elem*)el)->value;
 }
 
 void list_delete(struct map* this_map, int a){
@@ -33,7 +29,7 @@ void list_delete(struct map* this_map, int a){
 	return;
 }
 
-struct map* hash_create(int (*hashing)(data* key)){
+struct map* hash_create(int (*hashing)(data* key), int (*key_equal)(data* key_1, data* key_2)){
 	map* m;
 	if( (m = (map*)malloc(sizeof(hash))) == NULL){
 		perror("(hash_create, 41)malloc for m");
@@ -47,34 +43,47 @@ struct map* hash_create(int (*hashing)(data* key)){
 	m->get_prev = hash_prev;
 	m->first = hash_first;
 	m->last = hash_last;
+	m->end = hash_end;
 	hash* ha = (hash*) m;
 	if((ha->start = (map_enter**)malloc(H * sizeof(elem*))) == NULL)
 	{
 		perror("(hash_create, 46)malloc for ha->start");
 		return NULL;
 	}
+	int i;
+	elem** beg = (elem**)ha->start;
+	for (i = 0; i < H; i++)
+		ha->start[i] = NULL;
 	ha->hashing = hashing;
+	ha->equal = key_equal;
 	return (struct map*) m;
 }
 
+int unique(struct map* this_map, data* key){
+	struct map_enter* i;
+	for(i = this_map->first(this_map); i != this_map->end(this_map); i = this_map->get_next(this_map, i))
+		if(((hash*)this_map)->equal(((elem*)i)->value, key))
+			return 0;
+	return 1;
+}
+
 struct map_enter* hash_insert(struct map* this_map, data* ent){
-	elem* ent_new = (elem*)ent->value;
-	hash* m = (hash*) this_map;
-	elem** beg = (elem**)m->start;
-	int k = hashing(ent_new->key); //pointer to start of list
-	map_enter* next_el;
-	if(beg[k] != NULL)//list is not empty
-		next_el = beg[k]->next;
+	int k = ((hash*)this_map)->hashing(ent) % H; //pointer to start of list
+	map_enter* next_el = NULL;
+	elem** beg = (elem**)((hash*)this_map)->start;
+	next_el = (map_enter*)beg[k];
+	if(unique(this_map, ent))
+	{
+		beg[k] = NULL;
+		if((beg[k] = (elem*)malloc(sizeof(elem))) == NULL)
+			return NULL;
+		beg[k]->get_val = get_value;
+		beg[k]->next = next_el;
+		beg[k]->value = ent;
+		return (struct map_enter*)beg[k];
+	}
 	else
-		next_el = NULL;
-	if((beg[k] = (elem*)malloc(sizeof(elem))) == NULL)
-		perror("(hash_insert) malloc for el");
-	beg[k]->next = next_el;
-	beg[k]->value = malloc(sizeof(data));
-	*beg[k]->value = *ent_new->value;
-	beg[k]->key = malloc(sizeof(data));
-	*((int*)beg[k]->key) = *((int*)ent_new->key);
-	return (struct map_enter*)beg[k];
+		return NULL;
 }
 
 void hash_delete(struct map* this_map){
@@ -90,29 +99,29 @@ void hash_delete(struct map* this_map){
 struct map_enter* hash_search(struct map* this_map, data* key){
 	hash* m = (hash*) this_map;
 	elem** beg = (elem**)m->start;
-	elem* el = beg[hashing(key)];
-	while (el->key != key)
+	elem* el = beg[m->hashing(key)];
+	while (!(m->equal(key, el->value)))
 		el = (elem*)el->next;
 	return (struct map_enter*) el;
 }
 
 void hash_remove(struct map* this_map, struct map_enter* d){
 	elem* d_new = (elem*)d;
-	data* key = d_new->key;
+	data* key = d_new->value;
 	hash* m = (hash*) this_map;
 	if (m == NULL){
 		printf("This map doesn't exist");
 		return;
 	}
 	elem** beg = (elem**)m->start;
-	elem* el = beg[hashing(key)];
+	elem* el = beg[m->hashing(key)];
 	if (el == NULL){
 		printf ("Element with this key doesn't exist");
 		return;
 	}
 	elem* prev = NULL;
 
-	while ((*(int*)el->key) != (*(int*)key) || el == NULL){
+	while (!(m->equal(key, el->value)) || el == NULL){
 		elem* prev = el;
 		el = (elem*)el->next;
 	}
@@ -137,43 +146,51 @@ void hash_remove(struct map* this_map, struct map_enter* d){
 
 struct map_enter* hash_prev(struct map* this_map, struct map_enter* d){
 	elem* d_new = (elem*)d;
-	data* key = d_new->key;
-	int k = hashing(key);
+	data* key = d_new->value;
 	hash* m = (hash*)this_map;
+	int k = m->hashing(key);
 	elem** beg = (elem**)m->start;
-	if((*(int*)beg[k]->key) != (*(int*)key))
+	if(!(m->equal(beg[k]->value, d_new->value)))
 	{
 		elem* i = (elem*)beg[k]->next;
 		elem* prev = beg[k];
-		while((*(int*)i->key) != (*(int*)key))
+		while(!(m->equal(beg[k]->value, d_new->value)) && i->next != NULL)
 			i = (elem*)i->next;
-		return (struct map_enter*) prev;
+		if(!(m->equal(beg[k]->value, d_new->value)) && i->next == NULL)
+			return NULL;
+		else
+			return (struct map_enter*) prev;
 	}
 
 	k = (k + H - 1) % H;
-	while(beg[k] == NULL)
-		k = (k + H - 1) % H;
-	return (struct map_enter*)beg[k];
+	while(k > -1 && beg[k] == NULL)
+		k--;
+	if(k > -1)
+		return (struct map_enter*)beg[k];
+	else
+		return NULL;
 }
 
 struct map_enter* hash_next(struct map* this_map, struct map_enter* d){
 	elem* d_new = (elem*)d;
-	data* key = d_new->key;
+	data* key = d_new->value;
 	if (d_new->next != NULL)
 		return (struct map_enter*)d_new->next;
 
-	int k = (hashing(key) + 1) %H;
 	hash* m = (hash*)this_map;
+	int k = (m->hashing(key) + 1) %H;
 	elem** beg = (elem**)m->start;
 
-	while(beg[k] == NULL)
-		k=(k + 1) % H;
-	return (struct map_enter*)beg[k];
+	while(k < H && beg[k] == NULL)
+		k++;
+	if(k < H)
+		return (struct map_enter*)beg[k];
+	else
+		return NULL;
 }
 
 struct map_enter* hash_first(struct map* this_map){
-	hash* m = (hash*)this_map;
-	elem** beg = (elem**)m->start;
+	elem** beg = (elem**)((hash*)this_map)->start;
 	int k = 0;
 	while (k < H && beg[k] == NULL)
 		k++;
@@ -196,4 +213,8 @@ struct map_enter* hash_last(struct map* this_map){
 	while(i->next != NULL) //go to the end of list
 		i = (elem*)i->next;
 	return (struct map_enter*)i;
+}
+
+struct map_enter* hash_end(struct map* this_map){
+	return NULL;
 }
--- hash.h
+++ hash.h
@@ -1,26 +1,32 @@
 #ifndef __hash__
 	#define __hash__
 	#include "map.h"
+
+	#define H 3091
+
 	struct elem
 	{
-		_map_enter_methods;
+		_map_enter_methods;//get_val(struct map_enter* a);
 		struct map_enter* next; //the next element of the list.
-		data* value;
-		data* key;
+		data* value; //it's pair of a key and a value
+//		data* key;
 	}typedef elem;
+
+	data* get_value(struct map_enter* el);
 
 	struct hash
 	{
 		_map_methods;
 		int (*hashing)(data* a); //hash function
+		int (*equal)(data* key_1, data* key_2);
 		struct map_enter** start; //pointer to the pointer to the element with hash function equaling 0
 	}typedef hash;
 
 
-	int hashing (data* key);
-	void list_delete(struct map* this_map, int a);
 
-	struct map* hash_create( int (*hashing)(data* key));
+	struct map* hash_create(int (*hashing)(data* key), int (*key_equal)(data* key_1, data* key_2));
+
+	void list_delete(struct map* this_map, int a);
 	struct map_enter* hash_insert(struct map* this_map, data* ent);
 	void hash_delete(struct map* this_map);
 	struct map_enter* hash_search(struct map * this_map, data* key);
@@ -28,7 +34,9 @@
 	struct map_enter* hash_prev(struct map* this_map, struct map_enter* d);
 	struct map_enter* hash_next(struct map* this_map, struct map_enter* d);
 
+	int unique(struct map* this_map, data* key);
+
 	struct map_enter* hash_first(struct map* this_map);
 	struct map_enter* hash_last(struct map* this_map);
-
+	struct map_enter* hash_end(struct map* this_map);
 #endif
